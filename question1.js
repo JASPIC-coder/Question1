c44637a2888fb116e93495ca20174acfc8902f2d
JASPIC-coder <iradukundachristine27@gmail.com>
2025-03-04 00:21:03 -0800

    Create question1.js

---
+++ question1.js
@@ -0,0 +1,41 @@
+A) Function countRoles(users) {
+   const user={};
+   return user.filter(accountRoles.user)=>[user(accountRoles.user)||0,1]
+   console.log(countRoles(users))
+   }
+
+B) Function countRoles(str){
+   while (accountRoles.length===0) return 0
+  return user
+  .filter(accountRoles.user)=>[user(accountRoles.user)
+    .map(user.name)
+    const user=[
+    {name:'alice', roles:'admin'}
+    {name:'bora', roles:'student'}
+    {name:'tina', roles:'instructor'}
+  {name:'fina', roles:'student'}
+  {name:'bobo', roles:'student'}
+  {names:'bob', roles:'admin'}
+   {name:'keza', roles:'student'}
+  {name:'winnie', roles:'student'}
+  console.log(countRoles(str)
+              }
+C) Function findAdmins(users){
+   return user.filter(!array.isarray(roles.name==='admin'))
+                                     }
+D) Function findAdmins(users){
+  if(roles.length===0) return'No admins found'
+   return user
+  .filter(accountRoles.user)=>[user(accountRoles.user)
+    .map(user.name)
+     console.log(findAdmins(user));
+   }
+E) Function generateWelcomeMessagea(users){
+   return user.filter('Welcome',${user.name}!you are an, ${user.roles}
+   }
+F) Function generateWelcomeMessagea(users){
+   return user.map('Welcome',${user.name}!you are an, ${user.roles}
+  }
+G) The advantages of using array methods (map, filter) over traditional loops in this
+scenario it is because :
+it makes the codes more clear and concise which is also reduce high risks of errors
